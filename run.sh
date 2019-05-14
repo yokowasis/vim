633be850f7a2069654ecb78a9070b31f5ab5bb65
Wasis Haryo Sasoko <yokowasis@gmail.com>
2019-05-14 15:32:58 +0800

    curl

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-wget https://raw.githubusercontent.com/yokowasis/vim/master/yokowasis.vim
+curl https://raw.githubusercontent.com/yokowasis/vim/master/yokowasis.vim -o yokowasis.vim
 
 cp -f yokowasis.vim ~/.vim_runtime/my_configs.vim
 rm -f yokowasis.vim
 
-wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+curl https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -o monokai.vim
 mkdir ~/.vim/
 mkdir ~/.vim/colors/
 cp -f monokai.vim ~/.vim/colors/
 rm -f monokai.vim
+
